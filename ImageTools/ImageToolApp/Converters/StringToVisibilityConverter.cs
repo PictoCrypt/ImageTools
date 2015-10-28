eea05ccf8996de86b68e5265bb1fef571cb8e5af
Marius Schröder <schroeder.marius@t-online.de>
2015-10-29 00:49:26 +0100

    No Expander, No Image.

---
+++ ImageTools/ImageToolApp/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ImageToolApp.Converters
+{
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = value as string;
+            if (result != null)
+            {
+                if (!string.IsNullOrEmpty(result))
+                {
+                    return Visibility.Visible;
+                }
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
