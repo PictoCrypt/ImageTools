853d2b897c27d40ac9a79ea11c80240ef50261a1
Xrayf <stoppel13@msn.com>
2015-11-10 09:30:55 +0100

    Save

---
+++ ImageTools/UserControlClassLibrary/ImagePresenter/ImagePresenter.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace UserControlClassLibrary.ImagePresenter
+{
+    /// <summary>
+    /// Interaction logic for ImagePresenter.xaml
+    /// </summary>
+    public partial class ImagePresenter : UserControl
+    {
+        public ImagePresenter()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ImageSourceProperty = DependencyProperty.Register(
+            "ImageSource",
+            typeof(string),
+            typeof(ImagePresenter),
+            new PropertyMetadata(false));
+
+        public string ImageSource
+        {
+            get { return (string)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+    }
+}
