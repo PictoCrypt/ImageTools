781afd8f442e114bcd4df1da092a963f5dcd27c4
Xrayf <stoppel13@msn.com>
2016-03-02 13:40:26 +0100

    Added LaplaceFilter + Test

---
+++ ImageTools/FunctionLib/Filter/Laplace.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+
+namespace FunctionLib.Filter
+{
+    public class Laplace : IFilter
+    {
+        public Laplace(Bitmap image, int startbits, int endbits)
+        {
+            Image = image;
+            StartRange = startbits;
+            EndRange = endbits;
+        }
+
+        public Bitmap Image { get; set; }
+        public int GetValue(int x, int y)
+        {
+            if (Image == null)
+            {
+                throw new ArgumentNullException("'Image' can not be null.");
+            }
+            var pixel = Image.GetPixel(x, y);
+            var left = default(Color);
+            var up = default(Color);
+            var right = default(Color);
+            var down = default(Color);
+            var pixelCount = 4;
+
+            if (x <= 0)
+            {
+                pixelCount--;
+            }
+            else
+            {
+                left = Image.GetPixel(x - 1, y);
+            }
+            if (x >= Image.Width - 1)
+            {
+                pixelCount--;
+            }
+            else
+            {
+                right = Image.GetPixel(x + 1, y);
+            }
+            if (y <= 0)
+            {
+                pixelCount--;
+            }
+            else
+            {
+                up = Image.GetPixel(x, y - 1);
+            }
+            if (y >= Image.Height - 1)
+            {
+                pixelCount--;
+            }
+            else
+            {
+                down = Image.GetPixel(x, y + 1);
+            }
+
+            //work out the colours individually
+            var diff = CalculateDifference(pixel, left, right, up, down, pixelCount);
+
+            //return the results...
+            return diff.Sum(Math.Abs);
+        }
+
+        private int[] CalculateDifference(Color pixel, Color left, Color right, Color up, Color down, int pixelCount)
+        {
+            var result = new int[3];
+            result[0] = (pixel.R * pixelCount) - (left.R + right.R + up.R + down.R);
+            result[1] = (pixel.G * pixelCount) - (left.G + right.G + up.G + down.G);
+            result[1] = (pixel.B * pixelCount) - (left.B + right.B + up.B + down.B);
+            return result;
+        }
+
+        public int StartRange { get; set; }
+        public int EndRange { get; set; }
+    }
+
+    public interface IFilter
+    {
+        Bitmap Image { get; set; }
+        int GetValue(int x, int y);
+        int StartRange { get; set; }
+        int EndRange { get; set; }
+    }
+}
---
+++ ImageTools/FunctionLib/Steganography/FilterFirst.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using FunctionLib.Helper;
+using FunctionLib.Filter;
+
+namespace FunctionLib.Steganography
+{
+    public class FilterFirst : SteganographicAlgorithm
+    {
+        protected override LockBitmap Encrypt(LockBitmap src, byte[] value, int password = 0, int significantIndicator = 3)
+        {
+            //random short picker
+            // Messagesize
+            var random = new Random(password);
+            //Size in the first 32 Bit
+            //Embed - solange nicht fertig, nächsten Shot holen (Zufallszahl)
+            return null;
+        }
+
+        protected override byte[] Decrypt(LockBitmap src, int password = 0, int significantIndicator = 3)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string ChangeColor(string srcPath, Color color)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int MaxEncryptionCount(int squarePixels)
+        {
+            return MaxEncryptionCount(squarePixels, 3);
+        }
+
+        public int MaxEncryptionCount(int squarePixels, int leastSignificantBitIndicator)
+        {
+            // We are using the parameter leastSignificantBitIndicator each byte.
+            var lsbs = squarePixels * leastSignificantBitIndicator;
+            // Each character uses 8 bits.
+            var result = lsbs / 8;
+            return result;
+        }
+    }
+}
