1e31082ad3cdb91b38c9664d229d843bce2a5326
Xrayf <stoppel13@msn.com>
2016-03-17 14:03:58 +0100

    Added initial CommandPrompt Project

---
+++ ImageTools/ImageToolCommandPrompt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ImageToolCommandPrompt
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO App should be able to encode/decode via command prompt
+        }
+    }
+}
