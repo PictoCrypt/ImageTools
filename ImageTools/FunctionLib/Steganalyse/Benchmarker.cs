4b5cd0eb2b3dc3a9d1d50889eadd60204626e816
Marius <stoppel13@msn.com>
2016-04-20 20:57:32 +0200

    Comment

--- ImageTools/FunctionLib/Steganalyse/Benchmarker.cs
+++ ImageTools/FunctionLib/Steganalyse/Benchmarker.cs
@@ -3,6 +3,9 @@ using System.Drawing;
 
 namespace FunctionLib.Steganalyse
 {
+    /*
+    *   This code is based on the DIIT steganography project, which is available at the following address: http://diit.sourceforge.net/
+    */
     public class Benchmarker
     {
 //VARIABLES
