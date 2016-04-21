0a7dd2bf6d15929f588e95f060070b191fadb4bb
Marius <stoppel13@msn.com>
2016-04-21 09:51:03 +0200

    Initial Analysis Test

---
+++ ImageTools/UnitTests/FunctionLibTests/AnalysisTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing;
+using FunctionLib.Steganalyse;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.FunctionLibTests
+{
+    [TestClass]
+    public class AnalysisTest
+    {
+        [TestMethod]
+        public void SamplePairAnalysisTest()
+        {
+            string result;
+            using (var stego = new Bitmap(TestingConstants.NormalJpgImage))
+            {
+                var analyser = new StegAnalyser(false, true, false);
+                result = analyser.Run(stego);
+            }
+            Assert.IsFalse(string.IsNullOrEmpty(result));
+        }
+
+        [TestMethod]
+        public void RsAnalysisTest()
+        {
+            string result;
+            using (var stego = new Bitmap(TestingConstants.NormalJpgImage))
+            {
+                var analyser = new StegAnalyser(true, false, false);
+                result = analyser.Run(stego);
+            }
+            Assert.IsFalse(string.IsNullOrEmpty(result));
+        }
+
+
+        [TestMethod]
+        public void LaplaceGraphAnalysisTest()
+        {
+            string result;
+            using (var stego = new Bitmap(TestingConstants.NormalJpgImage))
+            {
+                var analyser = new StegAnalyser(false, false, true);
+                result = analyser.Run(stego);
+            }
+            Assert.IsFalse(string.IsNullOrEmpty(result));
+        }
+    }
+}
